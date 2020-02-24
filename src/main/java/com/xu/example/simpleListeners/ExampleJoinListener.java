038847466bb77ee055af09d6b6ce24e0054f0324
Felix Gerberding <gerberding.felix@gmail.com>
2020-02-24 09:46:41 +0100

    added advanced listener functionality

--- src/main/java/com/xu/example/simpleListeners/ExampleJoinListener.java
+++ src/main/java/com/xu/example/simpleListeners/ExampleJoinListener.java
@@ -1,10 +1,13 @@
 
 package com.xu.example.simpleListeners;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 public class ExampleJoinListener implements Listener {
 
@@ -20,6 +23,25 @@ public class ExampleJoinListener implements Listener {
 
         // Schickt ihm eine persönlich Willkommensnachricht
         joinedPlayer.sendMessage("Hallo, herzlich willkommen" + playerName + "!");
+
+        // ----------- //
+
+        // Mal angenommen wir möchten allen Spielern ein Willkommensgeschenk geben...
+
+        // Greift auf das Inventar des Spielers zu
+        PlayerInventory inventory = joinedPlayer.getInventory();
+
+        // Erstellt einen neuen Stack von 64 Diamanten (kein Datentyp!)
+        ItemStack itemstack = new ItemStack(Material.DIAMOND, 64);
+
+        // Überprüft ob der Spieler bereits einen Stack Diamanten im Inventar hat
+        if(!inventory.contains(itemstack)) {
+            // Wenn nicht wird er hinzugefügt
+            inventory.addItem(itemstack);
+            // Und eine Nachricht geschrieben
+            joinedPlayer.sendMessage("Hier ein kleines Willkommensgeschenk :)");
+        }
+
 
     }
 
